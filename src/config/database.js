7011907a3d248fc22e329e22437d6c01799e29dc
Charles Loureiro de Deus <charliedeus@gmail.com>
2019-10-16 21:20:47 -0300

    Configurando Sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 546720,
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    undersocred: true,
+    underscoredAll: true,
+  },
+};
