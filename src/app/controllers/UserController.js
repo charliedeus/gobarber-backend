5fbdb71b2728f3389754dc4009c33aa032f84508
Charles Loureiro de Deus <charliedeus@gmail.com>
2019-10-20 19:43:16 -0300

    Adding avatar in user model

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -69,12 +69,13 @@ class UserController {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, name, provider } = await user.update(req.body);
+    const { id, name, avatar_id, provider } = await user.update(req.body);
 
     return res.json({
       id,
       name,
       email,
+      avatar_id,
       provider,
     });
   }
