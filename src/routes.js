4c3b6228c96ac590c884a098b8bf43513677b7ba
Charles Loureiro de Deus <charliedeus@gmail.com>
2019-10-21 08:14:03 -0300

    Implementing provider schedule list

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -22,6 +23,7 @@ routes.put('/users', UserController.update);
 // Routes list
 routes.get('/providers', ProviderController.index);
 routes.get('/appointments', AppointmentController.index);
+routes.get('/schedules', ScheduleController.index);
 
 // Cadastra agendamentos
 routes.post('/appointments', AppointmentController.store);
