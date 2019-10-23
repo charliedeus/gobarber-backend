50869a9a1553ca81c33d598a5a4091faa7250068
Charles Loureiro de Deus <charliedeus@gmail.com>
2019-10-22 22:14:44 -0300

    Configuring template email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'e36485729e57a1',
+    pass: '1c772b59e6a44d',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com',
+  },
+};
